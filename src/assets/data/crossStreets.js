d76221958d6dcd4fd94936d4a7354efbff4aa1e3
rose-water <rachel.ulgado@gmail.com>
2021-10-10 21:50:17 -0700

    adding cross streets

---
+++ src/assets/data/crossStreets.js
@@ -0,0 +1,62 @@
+export const crossStreets = [
+  { 
+    street: "Doheny Drive", 
+    idx: 14.381000000000002 
+  },
+  { 
+    street: "La Cienega Boulevard", 
+    idx: 93.52745 
+  },
+  { 
+    street: "Fairfax Avenue", 
+    idx: 180.68232 
+  },
+  { 
+    street: "Highland Ave.", 
+    idx: 307.66841 
+  },
+  { 
+    street: "Vine St.", 
+    idx: 374.27960 
+  },
+  { 
+    street: "Highland Ave.", 
+    idx: 307.66841 
+  },
+  { 
+    street: "U.S. Rte. 101/Hollywood Fwy", 
+    idx: 442.04488 
+  },
+  { 
+    street: "Western Ave.", 
+    idx: 471.10667 
+  },
+  { 
+    street: "Vermont Ave.", 
+    idx: 567.89764 
+  },
+  { 
+    street: "Silver Lake Blvd.", 
+    idx: 713.7899999999988 
+  },
+  { 
+    street: "Alvarado St.", 
+    idx: 775.9789999999974 
+  },
+  { 
+    street: "Echo Park Ave.", 
+    idx: 810.9609999999967 
+  },
+  { 
+    street: "State Rte. 110/Harbor Fwy", 
+    idx: 909.4209999999945 
+  },
+  { 
+    street: "Figueroa St.", 
+    idx: 918.2079999999944 
+  },
+  { 
+    street: "Alameda St.", 
+    idx: 977.160000000001 
+  }
+]
