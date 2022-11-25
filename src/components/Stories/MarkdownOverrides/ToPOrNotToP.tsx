7440e324a31dc424e4556b5f46f2dbcd9c1e257c
rnelson2 <rnelson2@users.noreply.github.com>
2022-11-25 14:54:07 -0500

    troubleshooting markdown overrides trying to get them to work remotely 3

--- src/components/Stories/MarkdownOverrides/ToPOrNotToP.tsx
+++ src/components/Stories/MarkdownOverrides/ToPOrNotToP.tsx
@@ -1,13 +1,14 @@
 // By default the markdown-to-jsx wraps a most elements in paragraph tags. 
 //  This prevents that for images.
 const ToPOrNotToP = ({ children, ...props }: any) => {
+  console.log(children[0].type, children[0]?.type?.name);
   if (children[0]?.type === 'code' && children[0]?.props?.children) {
     return <div className="fullwidth-text"><p>{children[0]?.props?.children}</p></div>
   }
   if (children[0]?.type?.name === 'ModalImg' && children.length === 1) {
     return <div className="single-image">{children}</div>;
   }
-  const ParaComponent = (children[0]?.type?.name === 'img' || children[0]?.type?.name === 'code') ? 'div' : 'p'
+  const ParaComponent = (children[0]?.type?.name === 'ModalImg' || children[0]?.type?.name === 'img' || children[0]?.type?.name === 'code') ? 'div' : 'p'
   return <ParaComponent {...props}>{children}</ParaComponent>
 }
 
