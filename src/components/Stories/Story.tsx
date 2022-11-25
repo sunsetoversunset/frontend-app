7b97d38fca0e90c5d27370c45fb8a321e3911f51
rnelson2 <rnelson2@users.noreply.github.com>
2022-11-25 14:48:22 -0500

    troubleshooting markdown overrides trying to get them to work remotely 2

--- src/components/Stories/Story.tsx
+++ src/components/Stories/Story.tsx
@@ -148,6 +148,7 @@ const Story = () => {
                 },
                 ul: {
                   component: ImageList,
+                  props: {}
                 },
               }
             }}
