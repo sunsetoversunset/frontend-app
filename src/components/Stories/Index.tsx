9a7c51730e69e1a8585a1c541c2371872a00dee7
rnelson2 <rnelson2@users.noreply.github.com>
2023-07-26 22:11:52 -0400

    style adjustment to placement of background image on stories

--- src/components/Stories/Index.tsx
+++ src/components/Stories/Index.tsx
@@ -8,7 +8,10 @@ const Stories = () => {
   const unpublishedStoriesMetadata = useStoriesMetadataUnPublished();
   return (
     <Styled.Stories>
-      <Styled.HeaderImage url={`https://media.getty.edu/iiif/image/b5ae8080-6f5e-4178-a13a-5589446004a2/full/,${width}/0/default.jpg`}>
+      <Styled.HeaderImage
+        url={`https://media.getty.edu/iiif/image/b5ae8080-6f5e-4178-a13a-5589446004a2/full/,${width}/0/default.jpg`}
+        backgroundPosition='bottom center'
+      >
         <Styled.Title>Stories</Styled.Title>
       </Styled.HeaderImage>
 
