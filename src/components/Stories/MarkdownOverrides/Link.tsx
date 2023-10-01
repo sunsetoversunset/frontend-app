56241d7b4b89da77d46ab111f54042375d609db1
rnelson2 <rnelson2@users.noreply.github.com>
2023-10-01 14:22:07 -0400

    open story links in new tab

--- src/components/Stories/MarkdownOverrides/Link.tsx
+++ src/components/Stories/MarkdownOverrides/Link.tsx
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom';
 const AOrLink = ({ children, ...props }: any) => {
   return (props.href.includes('http')) 
     ? <a {...props}>{children}</a>
-    : <Link to={props.href} id={(children.type === 'sup') ? `footnote${props.href.substring(1)}` : undefined}>{children}</Link>
+    : <Link
+      to={props.href}
+      id={(children.type === 'sup') ? `footnote${props.href.substring(1)}` : undefined}
+      target={(children.type !== 'sup') ? '_blank' : undefined}
+      rel={(children.type !== 'sup') ? 'noopener noreferrer' : undefined}
+    >
+      {children}
+    </Link>
+  
 }
 
 export default AOrLink;
